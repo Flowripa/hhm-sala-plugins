28366b15f7c0ca86c81d925ca59844803aa659cb
Flowripa <128995095+Flowripa@users.noreply.github.com>
2023-03-26 13:24:29 -0500

    Update afk-monitor.js

--- src/hr/afk-monitor.js
+++ src/hr/afk-monitor.js
@@ -257,7 +257,7 @@ function kickInactivePlayer(playerId) {
  */
 function warnInactivePlayer(playerId) {
   let msg = `si usted no se mueve en los proximos ${room.getConfig('warnBefore')} segundos será expulsado.`
-  room.sendAnnouncement(msg, playerId, 0xFF0000, 'bold', 2);
+  room.sendAnnouncement(msg, playerId, 0xFF0000, 3);
 }
 
 room.onRoomLink = function onRoomLink() {
